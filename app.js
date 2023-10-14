856c69b33a4d856dbf92eff394c815efcccb4cc4
mayco91 <mayconitido@gmail.com>
2023-10-14 18:55:09 -0300

    creacion del repositorio

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const path = require("path")
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/home.html"))
+})
+
+app.use(express.static("public"))
+
+
+
+app.listen(5000,()=> {
+    console.log("servidor corriendo en puerto 5000")
+})
