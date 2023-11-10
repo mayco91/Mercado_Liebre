2ac944bd63f0403fb0a5dac06917202c9de66a80
mayco91 <mayconitido@gmail.com>
2023-11-10 08:34:36 -0300

    creacion rama formularios

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const app = express();
 const path = require("path")
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json());
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/home.html"))
 })
 
+app.get("/register",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+})
+
+app.post("/register",(req,res)=>{
+    console.log(req.body);
+    res.redirect("/")
+})
+
+app.post("/login",(req,res)=>{
+    console.log(req.body);
+    res.redirect("/")
+})
+
+
+
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+})
 app.use(express.static("public"))
 
 
